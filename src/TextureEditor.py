bb0dbe857aa89cacb7ebc2e04227110fdb104633
karstensensensen <61250140+karstensensensen@users.noreply.github.com>
2021-07-29 17:17:57 +0200

    implemented box draw mode

--- src/TextureEditor.py
+++ src/TextureEditor.py
@@ -44,6 +44,8 @@ class TextureEditor(ttk.Frame):
             PaletteData() for x in range(self.width)
         ] for y in range(self.height)]
 
+        self.current_image_data = None
+
         # holds older versions of [texture_data] to be restored if necessary
         self.undo_data = []
         self.redo_data = []
@@ -123,20 +125,13 @@ class TextureEditor(ttk.Frame):
         char_width, char_height = self.__charDimensions()
 
         self.draw.rectangle((pos[0] * char_width, pos[1] * char_height,
-               (pos[0] + 1) * char_width, (pos[1] + 1) * char_height), fill=background)
+                             (pos[0] + 1) * char_width, (pos[1] + 1) * char_height), fill=background)
 
         self.draw.text((pos[0] * char_width, pos[1] * char_height), char, font=self.font, fill=foreground)
 
     def __getFont(self, size):
         self.zoom = size
         return ImageFont.truetype('../Resources/SourceCodePro/SourceCodePro-Regular.ttf', size)
-
-    def __generateImage(self):
-        char_width, char_height = self.__charDimensions()
-
-        background = self.__backgroundColor()
-
-        return Image.fromarray(np.full((self.height * char_height, self.width * char_width, 4), background, dtype=np.uint8))
 
     def __charDimensions(self):
         ascent, descent = self.font.getmetrics()
@@ -145,26 +140,65 @@ class TextureEditor(ttk.Frame):
 
         return char_width, char_height
 
+    def __backgroundColor(self):
+        # get rgb_value of frame background color and map it from 0-65536 to 0-255, also darken it to create a border
+        return *[round(c_val / 65536 * 255 * 9 / 10) for c_val in self.canvas.winfo_rgb(self.canvas['background'])], 255
+
+    def __generateImage(self):
+        char_width, char_height = self.__charDimensions()
+
+        background = self.__backgroundColor()
+
+        return Image.fromarray(
+            np.full((self.height * char_height, self.width * char_width, 4), background, dtype=np.uint8))
+
+    def __tileCoord(self, x, y):
+        """
+        Returns coordinates x and y projected on a plane with a grid size equivalent to the font size
+        and the origin is the upper left corner of the texture image.
+        """
+
+        return floor((x - self.canvas.coords(self.img_id)[0]) / self.__charDimensions()[0]), \
+               floor((y - self.canvas.coords(self.img_id)[1]) / self.__charDimensions()[1])
+
+    def __getImageIndex(self, x, y):
+        """
+        Takes two coordinates and returns the two indices the two coordinates touch.
+        If they are not inside the image, None wil be returned
+        """
+
+        if self.canvas.coords(self.img_id)[0] < x < self.canvas.coords(self.img_id)[0] + self.img.width() and \
+                self.canvas.coords(self.img_id)[1] < y < self.canvas.coords(self.img_id)[1] + self.img.height():
+            return self.__tileCoord(x, y)
+
+        return None
+
+    def __limitValue(self, val, min_val, max_val):
+        """keeps [val] greater than or equal to [min_val] and less than or equal to [max_val]"""
+
+        assert min_val <= max_val
+
+        return min(max(min_val, val), max_val)
+
     def __drawData(self):
         """draws [texture_data] to the [texture_img]"""
 
         for y, row in enumerate(self.texture_data):
             for x, col in enumerate(row):
-                self.drawChar((x, y),
-                                col.character if col.character is not None else ' ',
-                                col.foreground_color.rgba() if col.foreground_color is not None else None,
-                                col.background_color.rgba() if col.background_color is not None else self.__backgroundColor())
+                if self.current_image_data is None or col != self.current_image_data[y][x]:
+                    self.drawChar((x, y),
+                                  col.character if col.character is not None else ' ',
+                                  col.foreground_color.rgba() if col.foreground_color is not None else None,
+                                  col.background_color.rgba() if col.background_color is not None else self.__backgroundColor())
+
+        self.current_image_data = deepcopy(self.texture_data)
 
     def __drawImage(self):
         """creates a image from [texture_img] on the canvas"""
 
-        self.img = ImageTk.PhotoImage(self.texture)
+        self.img.paste(self.texture)
 
         self.canvas.itemconfigure(self.img_id, image=self.img)
-
-    def __backgroundColor(self):
-        # get rgb_value of frame background color and map it from 0-65536 to 0-255, also darken it to create a border
-        return *[round(c_val / 65536 * 255 * 9 / 10) for c_val in self.canvas.winfo_rgb(self.canvas['background'])], 255
 
     def __zoom(self, step, event):
         if not self.moving:
@@ -180,6 +214,8 @@ class TextureEditor(ttk.Frame):
         self.texture = self.__generateImage()
         self.draw = ImageDraw.Draw(self.texture)
         self.img = ImageTk.PhotoImage(self.texture)
+
+        self.current_image_data = None
 
         self.__drawData()
         self.__drawImage()
@@ -209,20 +245,27 @@ class TextureEditor(ttk.Frame):
 
             self.undo_data.append(deepcopy(self.texture_data))
 
+        if self.mode == Modes.BOX:
+            self.box_begin = self.__tileCoord(event.x, event.y)
+            self.no_box_data = deepcopy(self.texture_data)
+
         self.__draw(event)
 
     def __drawEnd(self, event):
         if self.undo_data[-1] == self.texture_data:
             self.undo_data.pop()
 
+        if self.mode == Modes.BOX:
+            del self.box_begin
+            del self.no_box_data
+
     def __draw(self, event):
-        if self.canvas.coords(self.img_id)[0] < event.x < self.canvas.coords(self.img_id)[0] + self.img.width() and \
-                self.canvas.coords(self.img_id)[1] < event.y < self.canvas.coords(self.img_id)[1] + self.img.height():
+        if self.mode == Modes.PEN:
+            indices = self.__getImageIndex(event.x, event.y)
 
-            pos_x = floor((event.x - self.canvas.coords(self.img_id)[0]) / self.__charDimensions()[0])
-            pos_y = floor((event.y - self.canvas.coords(self.img_id)[1]) / self.__charDimensions()[1])
+            if indices is not None:
+                pos_x, pos_y = indices
 
-            if self.mode == Modes.PEN:
                 if self.draw_data.character is not None:
                     self.texture_data[pos_y][pos_x].character = self.draw_data.character
 
@@ -231,8 +274,29 @@ class TextureEditor(ttk.Frame):
 
                 if self.draw_data.background_color is not None:
                     self.texture_data[pos_y][pos_x].background_color = self.draw_data.background_color
-            elif self.mode == Modes.BOX:
-                print(event)
+
+                self.__drawData()
+                self.__drawImage()
+
+        elif self.mode == Modes.BOX:
+
+            crop_width = lambda val: self.__limitValue(val, 0, self.width)
+            crop_height = lambda val: self.__limitValue(val, 0, self.height)
+
+            pos_x, pos_y = self.__tileCoord(event.x, event.y)
+
+            diff_x = abs(crop_width(self.box_begin[0]) - crop_width(pos_x))
+            diff_y = abs(crop_height(self.box_begin[1]) - crop_height(pos_y))
+
+            # rectangle should be drawn from top left to bottom right
+            offset_x = min(crop_width(self.box_begin[0]), crop_width(pos_x))
+            offset_y = min(crop_height(self.box_begin[1]), crop_height(pos_y))
+
+            self.texture_data = deepcopy(self.no_box_data)
+
+            for x in range(diff_x + 1):
+                for y in range(diff_y + 1):
+                    self.texture_data[y + offset_y][x + offset_x] = self.draw_data
 
             self.__drawData()
             self.__drawImage()
@@ -246,21 +310,50 @@ class TextureEditor(ttk.Frame):
 
             self.undo_data.append(deepcopy(self.texture_data))
 
+        if self.mode == Modes.BOX:
+            self.box_begin = self.__tileCoord(event.x, event.y)
+            self.no_box_data = deepcopy(self.texture_data)
+
         self.__erase(event)
 
     def __eraseEnd(self, event):
         if self.undo_data[-1] == self.texture_data:
             self.undo_data.pop()
 
+        if self.mode == Modes.BOX:
+            del self.box_begin
+            del self.no_box_data
+
     def __erase(self, event):
-        if self.canvas.coords(self.img_id)[0] < event.x < self.canvas.coords(self.img_id)[0] + self.img.width() and \
-                self.canvas.coords(self.img_id)[1] < event.y < self.canvas.coords(self.img_id)[1] + self.img.height():
+        if self.mode == Modes.PEN:
 
-            pos_x = floor((event.x - self.canvas.coords(self.img_id)[0]) / self.__charDimensions()[0])
-            pos_y = floor((event.y - self.canvas.coords(self.img_id)[1]) / self.__charDimensions()[1])
+            indices = self.__getImageIndex(event.x, event.y)
 
-            if self.mode == Modes.PEN:
+            if indices is not None:
+                pos_x, pos_y = indices
+
                 self.texture_data[pos_y][pos_x] = PaletteData()
+
+                self.__drawData()
+                self.__drawImage()
+        elif self.mode == Modes.BOX:
+
+            crop_width = lambda val: self.__limitValue(val, 0, self.width)
+            crop_height = lambda val: self.__limitValue(val, 0, self.height)
+
+            pos_x, pos_y = self.__tileCoord(event.x, event.y)
+
+            diff_x = abs(crop_width(self.box_begin[0]) - crop_width(pos_x))
+            diff_y = abs(crop_height(self.box_begin[1]) - crop_height(pos_y))
+
+            offset_x = min(crop_width(self.box_begin[0]), crop_width(pos_x))
+            offset_y = min(crop_height(self.box_begin[1]), crop_height(pos_y))
+
+            self.texture_data = deepcopy(self.no_box_data)
+
+            for x in range(diff_x + 1):
+                for y in range(diff_y + 1):
+                    self.texture_data[y + offset_y][x + offset_x] = PaletteData()
 
             self.__drawData()
             self.__drawImage()
@@ -275,6 +368,8 @@ if __name__ == '__main__':
 
     t = TextureEditor(root, 40, 40, 40)
     t.grid(column=0, row=0, sticky='NW')
+
+    t.mode = Modes.BOX
 
     t.draw_data = PaletteData('a', RGBA((255, 0, 0)), RGBA((0, 255, 0)))
 
